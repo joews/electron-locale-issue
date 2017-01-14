38021933d17a354394e85757dc59648becc545dd
Joe Whitfield-Seed <joe.ws@ripjar.com>
2017-01-14 00:34:23 +0000

    Add repro case for https://github.com/electron-userland/electron-packager/issues/558

--- main.js
+++ main.js
@@ -7,11 +7,20 @@ const BrowserWindow = electron.BrowserWindow
 const path = require('path')
 const url = require('url')
 
+console.log(new Date().toLocaleString())
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
 function createWindow () {
+
+  //
+  // repro locale issue: https://github.com/electron-userland/electron-packager/issues/558
+  //
+  electron.dialog.showMessageBox({  message: "[" + app.getLocale() + "] " + new Date().toLocaleString()  })
+
+
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
